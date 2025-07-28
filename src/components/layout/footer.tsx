616375da0d6cb355cc9bf4287df3a4e80e892418
UtsavMavani <utsavmavani000@gmail.com>
2025-07-29 00:52:49 +0530

    Feat: logo and favicon

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <Typography className="flex items-center" variant="body3">
           <Copyright className="mr-1 inline-block h-4 w-4" />
           {new Date().getFullYear()} |&nbsp; Developed
-          &nbsp;with ❤️️ by Utsav
+          &nbsp;with ❤️️ by Utsav Mavani
         </Typography>
       </div>
     </footer>
